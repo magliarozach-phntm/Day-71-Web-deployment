c0aa4fafc2590f1f123363f00244c31a63751858
Zach Magliaro <MagliaroZach@gmail.com>
2026-07-21 11:34:51 -0400

    Update port

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def contact():
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
